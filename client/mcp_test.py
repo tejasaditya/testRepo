0f512ca72ceb8f70828429329d33b838fdf62787
Tejas Aditya <tejasaditya10@gmail.com>
2025-07-30 11:25:29 -0700

    Delete client/mcp_test.py

--- client/mcp_test.py
+++
@@ -1,6 +0,0 @@
-"""
-This is a test file to demonstrate code commit using IBM API Agent which is consuming GitHub's remote MCP server.
-"""
-
-if __name__ == '__main__':
-    print("Welcome to IBM API Agent!")
