ca3b624575a09c4f317573fba8e58e7ed511a71f
Tejas Aditya <tejasaditya10@gmail.com>
2025-07-30 11:14:20 -0700

    Testing MCP client

---
+++ client/mcp_test.py
@@ -0,0 +1,6 @@
+"""
+This is a test file to demonstrate code commit using IBM API Agent which is consuming GitHub's remote MCP server.
+"""
+
+if __name__ == '__main__':
+    print("Welcome to IBM API Agent!")
